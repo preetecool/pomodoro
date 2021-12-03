7c914f727e103dcdc33239be197a0d1abcad581d
Preet <preet@creal.ca>
2021-12-03 14:27:35 -0500

    settings page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 	return <div className="App">
 		<h1 className="AppTitle">pomodoro</h1>
 		<Clock/>
-
+		<Settings/>
 
 	</div>;
 }
--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../Css/settings.css"
+import icon from "../../assets/icon-settings.svg";
 
 const Settings = () => {
-	return <div></div>;
+	return <div>
+		<img className="settings-icon" src={icon}></img>
+		<div className="modal"></div>
+	</div>;
 };
 
 export default Settings;
