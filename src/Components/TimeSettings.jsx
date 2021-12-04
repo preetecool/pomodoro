5b26f211f7f6f91bdb9330024f65f948ec9220da
Preet <preet@creal.ca>
2021-12-04 13:50:25 -0500

    settings module changes

--- src/Components/TimeSettings.jsx
+++ src/Components/TimeSettings.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import upArrow from "../../assets/icon-arrow-up.svg"
+import downArrow from "../../assets/icon-arrow-down.svg"
+
 
 const TimeSettings = () => {
     return (
@@ -8,18 +11,27 @@ const TimeSettings = () => {
 				<div className="time-settings">
 
 					<div className="input-wrapper">
+						
 						<h4 className ="input-title">pomodoro</h4>
-						<input type="number" name="pomodoro"></input>
+						<input type="number" name="pomodoro" placeholder="00 mins"></input>
+
 					</div>
 
 					<div className="input-wrapper">
 						<h4 className ="input-title">short break</h4>
-						<input type="number" name="pomodoro"></input>
+						
+						<input type="number" name="pomodoro" placeholder="00 mins"></input>
+
+						<div className="arrow-wrapper">
+							<img className="arrow"src={upArrow}/>
+							<img  className="arrow"src={downArrow}/>
+						</div>
+
 					</div>
 
 					<div className="input-wrapper">
 						<h4 className ="input-title">long break</h4>
-						<input className ="time-input"type="number" name="long-break"></input>
+						<input className ="time-input"type="number" name="long-break" placeholder="00 mins"></input>
 					</div>
 					
 				</div>
