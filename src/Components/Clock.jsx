c2bf132d4c35221183a035256bd8d5dd559c9a09
Preet <preet@creal.ca>
2021-12-12 17:42:58 -0500

    adjusting useEffect of timer

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -11,13 +11,36 @@ const Clock = () => {
 	const {time, setTime} = useContext(pomodoroContext);
 
 	const [seconds, setSeconds] = useState(0)
+	const [minutes, setMinutes] = useState(0)
 
 	const timerSeconds = seconds < 10 ? `0${seconds}`: seconds
 
 	useEffect(()=> {
 		 
-		time.map((el, index) => {
 
+		time.filter( e => e.active === true).map((el, index) => {
+			if(el.minutes !== 0) {
+				setSeconds(59)
+				const timer = () => {
+					setInterval(() => {
+						setSeconds(seconds => seconds - 1)
+					},1000)
+					// if(seconds === 0 && el.minutes !==0) {
+					// 	setTime({...el, minutes: minutes - 1})
+					// 	setSeconds(seconds => seconds - 1)
+
+					// }
+				}
+				timer();
+				return clearInterval(timer);
+			}
+			
+		})
+		
+		// set seconds to 59
+		//every second 59 - 1
+		// when seconds reaches 0, time[index].minutes - 1 until minutes reaches 0.
+		
 			
 	},[])
 	
